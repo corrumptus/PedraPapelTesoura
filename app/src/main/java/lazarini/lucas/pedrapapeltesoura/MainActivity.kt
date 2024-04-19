ad634f47a595db7776ae3cb1371da67812a3ecb3
corrumptus <lazarinilucas42@gmail.com>
2024-04-18 23:43:18 -0300

    adding the button click event handler that will begin the game

--- app/src/main/java/lazarini/lucas/pedrapapeltesoura/MainActivity.kt
+++ app/src/main/java/lazarini/lucas/pedrapapeltesoura/MainActivity.kt
@@ -1,8 +1,10 @@
 package lazarini.lucas.pedrapapeltesoura
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import lazarini.lucas.pedrapapeltesoura.databinding.ActivityMainBinding
+import lazarini.lucas.pedrapapeltesoura.utils.IntentMessages.QUANTIDADE_BOTS
 
 class MainActivity : AppCompatActivity() {
     private val activityMainBinding: ActivityMainBinding by lazy {
@@ -12,5 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
+
+        activityMainBinding.vs1botBt.setOnClickListener {
+            Intent(this, JogoActivity::class.java).apply {
+                putExtra(QUANTIDADE_BOTS, 1)
+                startActivity(this)
+            }
+        }
+
+        activityMainBinding.vs2botBt.setOnClickListener {
+            Intent(this, JogoActivity::class.java).apply {
+                putExtra(QUANTIDADE_BOTS, 2)
+                startActivity(this)
+            }
+        }
     }
 }
