58d3246f6866737c003a20da02f5a1895125a657
corrumptus <lazarinilucas42@gmail.com>
2024-04-18 23:02:42 -0300

    changing the form that contentView is being get to viewBinding tecnique

--- app/src/main/java/lazarini/lucas/pedrapapeltesoura/MainActivity.kt
+++ app/src/main/java/lazarini/lucas/pedrapapeltesoura/MainActivity.kt
@@ -2,10 +2,15 @@ package lazarini.lucas.pedrapapeltesoura
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import lazarini.lucas.pedrapapeltesoura.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainBinding.root)
     }
 }
