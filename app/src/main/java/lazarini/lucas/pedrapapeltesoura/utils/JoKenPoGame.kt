68ab442eb4ba57e578fb8005cfdc708f824efbe8
corrumptus <lazarinilucas42@gmail.com>
2024-04-24 15:49:33 -0300

    adding a function that returns the string of the winners

--- app/src/main/java/lazarini/lucas/pedrapapeltesoura/utils/JoKenPoGame.kt
+++ app/src/main/java/lazarini/lucas/pedrapapeltesoura/utils/JoKenPoGame.kt
@@ -35,19 +35,19 @@ class JoKenPoGame(val version: JoKenPoVersion) {
 
     private fun getResult2Bots(playerChoose: JoKenPoPlayType): JoKenPoResult {
         val playerChooseTheCounterPlayOfTheAllBots = playerChoose == escolhaBot1.counter()
-                && playerChoose == escolhaBot2.counter()
+            && playerChoose == escolhaBot2.counter()
 
         if (playerChooseTheCounterPlayOfTheAllBots)
             return JoKenPoResult.VITORIA
 
         val playerChooseTheSamePlayAsOtherBots = playerChoose == escolhaBot1
-                && playerChoose == escolhaBot2
+            && playerChoose == escolhaBot2
 
         val playerChooseTheSamePlayAsOtherBot1AndNotLoseForTheBot2 = playerChoose == escolhaBot1
-                && playerChoose == escolhaBot2.counter()
+            && playerChoose == escolhaBot2.counter()
 
         val playerChooseTheSamePlayAsOtherBot2AndNotLoseForTheBot1 = playerChoose == escolhaBot2
-                && playerChoose == escolhaBot1.counter()
+            && playerChoose == escolhaBot1.counter()
 
         if (
             playerChooseTheSamePlayAsOtherBots ||
@@ -57,6 +57,25 @@ class JoKenPoGame(val version: JoKenPoVersion) {
             return JoKenPoResult.EMPATE
 
         return JoKenPoResult.DERROTA
+    }
+
+    fun winners(): String {
+        if (result == null)
+            return ""
+
+        if (result == JoKenPoResult.VITORIA)
+            return "Player"
+
+        if (result == JoKenPoResult.EMPATE)
+            return "Player" +
+                (if (playerChoose == escolhaBot1) ", Bot 1" else "") +
+                (if (playerChoose == escolhaBot2) ", Bot 2" else "")
+
+        if (escolhaBot1 == escolhaBot2)
+            return "Bot 1, Bot 2"
+
+        return if (escolhaBot1 == escolhaBot2.counter()) "Bot 1"
+            else "Bot 2"
     }
 
     fun getEscolhaBot1(): JoKenPoPlayType {
