673b2bcce54419919fd23d0c1733439dd3cc9d1a
corrumptus <lazarinilucas42@gmail.com>
2024-04-24 15:30:29 -0300

    adding state machine transitions pattern to the jokenpo play type enum

--- app/src/main/java/lazarini/lucas/pedrapapeltesoura/utils/JokenPoTypes.kt
+++ app/src/main/java/lazarini/lucas/pedrapapeltesoura/utils/JokenPoTypes.kt
@@ -3,7 +3,23 @@ package lazarini.lucas.pedrapapeltesoura.utils
 enum class JoKenPoPlayType(val play: String) {
     PEDRA("\uD83D\uDC4A"),
     PAPEL("✋"),
-    TESOURA("✌\uFE0F")
+    TESOURA("✌\uFE0F");
+
+    fun counter(): JoKenPoPlayType {
+        return when(this) {
+            JoKenPoPlayType.PEDRA -> JoKenPoPlayType.PAPEL
+            JoKenPoPlayType.PAPEL -> JoKenPoPlayType.TESOURA
+            JoKenPoPlayType.TESOURA -> JoKenPoPlayType.PEDRA
+        }
+    }
+
+    fun countered(): JoKenPoPlayType {
+        return when(this) {
+            JoKenPoPlayType.PEDRA -> JoKenPoPlayType.TESOURA
+            JoKenPoPlayType.PAPEL -> JoKenPoPlayType.PEDRA
+            JoKenPoPlayType.TESOURA -> JoKenPoPlayType.PAPEL
+        }
+    }
 }
 
 enum class JoKenPoVersion(val version: Int) {
