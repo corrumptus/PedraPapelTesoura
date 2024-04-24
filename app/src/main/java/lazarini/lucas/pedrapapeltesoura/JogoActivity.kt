040859317f0ff96867248a6aa1e9cbe69a8512c5
corrumptus <lazarinilucas42@gmail.com>
2024-04-24 15:57:37 -0300

    adding and instantiating the game class based on the jokenpo version received from the main activity

--- app/src/main/java/lazarini/lucas/pedrapapeltesoura/JogoActivity.kt
+++ app/src/main/java/lazarini/lucas/pedrapapeltesoura/JogoActivity.kt
@@ -3,11 +3,16 @@ package lazarini.lucas.pedrapapeltesoura
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import lazarini.lucas.pedrapapeltesoura.databinding.ActivityJogoBinding
+import lazarini.lucas.pedrapapeltesoura.utils.IntentMessages.QUANTIDADE_BOTS
+import lazarini.lucas.pedrapapeltesoura.utils.JoKenPoGame
+import lazarini.lucas.pedrapapeltesoura.utils.JoKenPoVersion
 
 class JogoActivity : AppCompatActivity() {
     private val activityJogoBinding: ActivityJogoBinding by lazy {
         ActivityJogoBinding.inflate(layoutInflater)
     }
+
+    private lateinit var game: JoKenPoGame
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +21,7 @@ class JogoActivity : AppCompatActivity() {
         activityJogoBinding.toolbarIn.apply {
             setSupportActionBar(toolbar)
         }
+
+        game = JoKenPoGame(intent.getSerializableExtra(QUANTIDADE_BOTS) as JoKenPoVersion)
     }
 }
