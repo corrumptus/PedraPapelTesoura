151135731467137417fc9852b988fb2a17ec95f6
corrumptus <lazarinilucas42@gmail.com>
2024-04-24 15:59:53 -0300

    creating a click handler to the game

--- app/src/main/java/lazarini/lucas/pedrapapeltesoura/JogoActivity.kt
+++ app/src/main/java/lazarini/lucas/pedrapapeltesoura/JogoActivity.kt
@@ -1,11 +1,13 @@
 package lazarini.lucas.pedrapapeltesoura
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import lazarini.lucas.pedrapapeltesoura.databinding.ActivityJogoBinding
 import lazarini.lucas.pedrapapeltesoura.utils.IntentMessages.QUANTIDADE_BOTS
 import lazarini.lucas.pedrapapeltesoura.utils.JoKenPoGame
+import lazarini.lucas.pedrapapeltesoura.utils.JoKenPoPlayType
 import lazarini.lucas.pedrapapeltesoura.utils.JoKenPoVersion
 
 class JogoActivity : AppCompatActivity() {
@@ -29,5 +31,33 @@ class JogoActivity : AppCompatActivity() {
             activityJogoBinding.bot2Tv.visibility = View.GONE
             activityJogoBinding.escolhaBot2.visibility = View.GONE
         }
+
+        activityJogoBinding.pedraBt.setOnClickListener {
+            playerPlayHandler(JoKenPoPlayType.PEDRA)
+        }
+
+        activityJogoBinding.papelBt.setOnClickListener {
+            playerPlayHandler(JoKenPoPlayType.PAPEL)
+        }
+
+        activityJogoBinding.tesouraBt.setOnClickListener {
+            playerPlayHandler(JoKenPoPlayType.TESOURA)
+        }
+    }
+
+    private fun playerPlayHandler(play: JoKenPoPlayType) {
+        activityJogoBinding.escolhaBot1.text = game.getEscolhaBot1().play
+        activityJogoBinding.escolhaBot1.setBackgroundColor(Color.parseColor("#334155"))
+
+        if (game.version == JoKenPoVersion.VS_2_BOT) {
+            activityJogoBinding.escolhaBot2.text = game.getEscolhaBot2().play
+            activityJogoBinding.escolhaBot2.setBackgroundColor(Color.parseColor("#334155"))
+        }
+
+        activityJogoBinding.escolhaPlayer.text = play.play
+        activityJogoBinding.escolhaPlayer.setBackgroundColor(Color.parseColor("#334155"))
+
+        activityJogoBinding.resultadoTv.text = game.getResult(play).result
+        activityJogoBinding.vencedoresTv.text = game.winners()
     }
 }
