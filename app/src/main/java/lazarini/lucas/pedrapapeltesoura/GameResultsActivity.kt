f5fe0289f78b4fd738706d9de82558d8a3b66bf6
corrumptus <lazarinilucas42@gmail.com>
2024-05-06 20:43:26 -0300

    initing the game results activity

---
+++ app/src/main/java/lazarini/lucas/pedrapapeltesoura/GameResultsActivity.kt
@@ -0,0 +1,16 @@
+package lazarini.lucas.pedrapapeltesoura
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import lazarini.lucas.pedrapapeltesoura.databinding.ActivityGameResultsBinding
+
+class GameResultsActivity : AppCompatActivity() {
+    private val activityGameResultBinding: ActivityGameResultsBinding by lazy {
+        ActivityGameResultsBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activityGameResultBinding.root)
+    }
+}
