993e722b681b2d8854b302dde855db4954053265
corrumptus <lazarinilucas42@gmail.com>
2024-05-07 00:34:10 -0300

    creating the tile game result adapter from the given game results

--- app/src/main/java/lazarini/lucas/pedrapapeltesoura/GameResultsActivity.kt
+++ app/src/main/java/lazarini/lucas/pedrapapeltesoura/GameResultsActivity.kt
@@ -2,16 +2,29 @@ package lazarini.lucas.pedrapapeltesoura
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import lazarini.lucas.pedrapapeltesoura.adapter.GameResultAdapter
 import lazarini.lucas.pedrapapeltesoura.databinding.ActivityGameResultsBinding
+import lazarini.lucas.pedrapapeltesoura.utils.IntentMessages.GAME_RESULTS
+import lazarini.lucas.pedrapapeltesoura.utils.JoKenPoGameResult
 
 class GameResultsActivity : AppCompatActivity() {
     private val activityGameResultBinding: ActivityGameResultsBinding by lazy {
         ActivityGameResultsBinding.inflate(layoutInflater)
     }
 
+    private lateinit var gameResultsAdapter: GameResultAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityGameResultBinding.root)
+
+        val gameResults = intent.getParcelableArrayExtra(GAME_RESULTS) as Array<JoKenPoGameResult>?
+
+        gameResultsAdapter = GameResultAdapter(
+            this,
+            if (gameResults != null) gameResults.toList()
+            else listOf()
+        )
 
         activityGameResultBinding.toolbarIn.apply {
             setSupportActionBar(toolbar)
