db0781fe3694325d8041363cedde7187ca5c6a71
corrumprus <lazarinilucas42@gmail.com>
2024-05-08 08:39:19 -0300

    adding lagarto and spock play types

--- app/src/main/java/lazarini/lucas/pedrapapeltesoura/utils/JokenPoTypes.kt
+++ app/src/main/java/lazarini/lucas/pedrapapeltesoura/utils/JokenPoTypes.kt
@@ -3,21 +3,27 @@ package lazarini.lucas.pedrapapeltesoura.utils
 enum class JoKenPoPlayType(val play: String) {
     PEDRA("\uD83D\uDC4A"),
     PAPEL("✋"),
-    TESOURA("✌\uFE0F");
+    TESOURA("✌\uFE0F"),
+    LAGARTO("\uD83E\uDD8E"),
+    SPOCK("\uD83D\uDD96");
 
-    fun counter(): JoKenPoPlayType {
+    fun counters(): List<JoKenPoPlayType> {
         return when(this) {
-            JoKenPoPlayType.PEDRA -> JoKenPoPlayType.PAPEL
-            JoKenPoPlayType.PAPEL -> JoKenPoPlayType.TESOURA
-            JoKenPoPlayType.TESOURA -> JoKenPoPlayType.PEDRA
+            PEDRA -> listOf(PAPEL, SPOCK)
+            PAPEL -> listOf(TESOURA, LAGARTO)
+            TESOURA -> listOf(PEDRA, SPOCK)
+            LAGARTO -> listOf(TESOURA, PEDRA)
+            SPOCK -> listOf(PAPEL, LAGARTO)
         }
     }
 
-    fun countered(): JoKenPoPlayType {
+    fun countereds(): List<JoKenPoPlayType> {
         return when(this) {
-            JoKenPoPlayType.PEDRA -> JoKenPoPlayType.TESOURA
-            JoKenPoPlayType.PAPEL -> JoKenPoPlayType.PEDRA
-            JoKenPoPlayType.TESOURA -> JoKenPoPlayType.PAPEL
+            PEDRA -> listOf(TESOURA, LAGARTO)
+            PAPEL -> listOf(PEDRA, SPOCK)
+            TESOURA -> listOf(PAPEL, LAGARTO)
+            LAGARTO -> listOf(PAPEL, SPOCK)
+            SPOCK -> listOf(PEDRA, TESOURA)
         }
     }
 }
